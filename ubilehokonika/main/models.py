10f4c288784770f61afc47259e2c5d6b01111e83
Daniel Fajt <daniel29se@gmail.com>
2021-02-13 22:05:43 -0800

    final homepage, translations, layouts, project skeleton

--- ubilehokonika/main/models.py
+++ ubilehokonika/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
+from datetime import datetime
+
 
 # Create your models here.
+
+class GlobalSettings(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    num_of_announcements = models.IntegerField()
+
+
+class Announcement(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cs = models.CharField(max_length=400)
+    en = models.CharField(max_length=400, blank=True)
+    de = models.CharField(max_length=400, blank=True)
+
+    def __str__(self):
+        return self.cs
--- ubilehokonika/main/views.py
+++ ubilehokonika/main/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from datetime import datetime
+from .models import Announcement
 
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext as _, get_language
 
 
 # Create your views here.
 
 
 def home(request):
+    print('Aktualni jazyk je:', get_language())
+    current_i18n = str(get_language())
+    announcements_query = Announcement.objects.values_list(current_i18n, 'created_at').order_by('created_at')[:4]
+    announcements = [{"msg": x[0], "date": x[1].strftime('%d.%m.%Y')} for x in announcements_query]
+    print(announcements)
     return render(
         request=request,
         template_name='base/home.html',
-        context={"current_year": datetime.now().year}
+        context={"current_year": datetime.now().year,
+                 "announcements": announcements}
     )
